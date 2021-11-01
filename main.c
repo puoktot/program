5e449f967697c3f1c6a1cc9c10ce0b91185753c4
puoktot <79037821+puoktot@users.noreply.github.com>
2021-11-01 17:19:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*here i will take you to 2 variable*/
+    int num1, num2;
+    printf("Enter num1:");
+    scanf("&d", &num1);
+    printf("Enter num2:");
+    scanf("&d", &num2); /* Don't forget to use "," at the end of the each line","*/
+    /* now watch it carefully how am using the operator*/
+    if(num1>num2) /* nume1 is greater than num2*/
+        printf("num1 is greater than num2");
+} /* Now build the application*/
